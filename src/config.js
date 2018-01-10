1f19a84d4d0f2118e2d168a720f44b860d1d6c45
Austin Fisher <AFish622@gmail.com>
2018-01-10 12:53:20 -0800

    updated config variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
-export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+// export const API_BASE_URL =
+//     process.env.REACT_APP_API_BASE_URL || 'https://hopinion-server.herokuapp.com/api';
+//     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+
 
 // in netlify this env variables exists
 // REACT_APP_API_BASE_URL
 // set it equal to your Heroku url
+
+
+export const API_BASE_URL = process.env.NODE_ENV == 'production'
+    ? 'https://hopinion-server.herokuapp.com/api' 
+    : 'http://localhost:8080/api'; 
